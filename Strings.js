f4ff26c4aed65a5ef3e594430874884b37bb5745
Ansh Sharma <122905517+AnshSharma16@users.noreply.github.com>
2024-08-25 18:25:27 +0530

    Strings.js

--- Strings.js
+++ Strings.js
@@ -5,4 +5,16 @@ const game=new String("TicTacToe")
 console.log(game[0])
 console.log(game.__proto__)
 console.log(game.length)
-console.log(game.toUpperCase)
+console.log(game.toUpperCase
+console.log(game.charAt(2))
+console.log(game.indexOf('i'))
+const newString=game.substring(0,4)
+console.log(newString)
+const anotherString=game.slice(-7,4)
+console.log(anotherString)
+const sp=" Ansh "
+console.log(sp.trim())
+const url="https://github.com/AnshSharma16/JavascriptTutorial/edit/main/Strings.js"
+console.log(url.replace('edit','change'))
+console.log(url.includes('sundar'))
+console.log(url.split('/'))
