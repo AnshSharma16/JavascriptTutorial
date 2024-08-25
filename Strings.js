6fa4dc89ffe950b26b9ea2af086bf5534b8b4728
Ansh Sharma <122905517+AnshSharma16@users.noreply.github.com>
2024-08-25 17:55:42 +0530

    Commit

--- Strings.js
+++ Strings.js
@@ -1,3 +1,8 @@
 const name="Ansh"
 const age=21
 console.log(`I am ${name}, ${age} years old`)
+const game=new String("TicTacToe")
+console.log(game[0])
+console.log(game.__proto__)
+console.log(game.length)
+console.log(game.toUpperCase)
