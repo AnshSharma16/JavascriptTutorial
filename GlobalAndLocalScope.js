1105fec079fc3e505c086994401428ce21ac174c
Ansh Sharma <122905517+AnshSharma16@users.noreply.github.com>
2024-08-29 02:42:44 +0530

    Push

---
+++ GlobalAndLocalScope.js
@@ -0,0 +1,43 @@
+/*var c=300
+if(true){
+    let a=10
+    const b=20
+    c=30
+}
+console.log(c)//30
+console.log(a)//a is not defined
+console.log(b)//b is not defined*/
+
+/*let c=300
+if(true){
+    let a=10
+    const b=20
+    c=30
+}
+console.log(c)//30
+console.log(a)//a is not defined
+console.log(b)//b is not defined*/
+
+/*let c=300
+if(true){
+    let a=10
+    const b=20
+    let c=30
+}
+console.log(c)//300
+console.log(a)//a is not defined
+console.log(b)//b is not defined*/
+
+function one(){
+    const username='Ansh'
+
+    function two(){
+        const website='YouTube'
+        console.log(username)
+    }
+    console.log(website)
+    two()
+}
+
+
+
